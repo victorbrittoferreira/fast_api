ae85aab5e2c24b5011ea3aff49025e34c45d2890
Victor de Britto Ferreira <victorbrittoferreira@gmail.com>
2022-12-18 22:19:57 -0300

    create the endpoints

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import databases
+from fastapi import FastAPI, Request
 import sqlalchemy
 
 DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/store"
 
-data = databases.Database(DATABASE_URL)
+database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
 books = sqlalchemy.Table(
@@ -13,3 +14,33 @@ books = sqlalchemy.Table(
     sqlalchemy.Column("title", sqlalchemy.String),
     sqlalchemy.Column("author", sqlalchemy.String),
 )
+
+engine = sqlalchemy.create_engine(DATABASE_URL)
+metadata.create_all()
+
+app = FastAPI()
+
+#Middleware
+@app.on_event("startup")
+async def startup():
+    await database.connect
+
+@app.on_event("shutodwn")
+async def shutodwn():
+    await database.disconnect
+
+@app.get("/books/")
+async def read_books():
+    query = books.select()
+    return await database.fetch_all(query)
+
+
+@app.post("/books/")
+async def create_books(request: Request):
+    data = await request.json()
+    #...(title = data.get("title"), author=data.get("author"))
+    query = books.insert().values(**data)
+    last_record_id = await database.execute(query)
+    
+    return {"id": last_record_id}
+    
