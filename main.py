020a58153fce926330371af7a228d61dfbd21244
Victor de Britto Ferreira <victorbrittoferreira@gmail.com>
2022-12-18 21:51:38 -0300

    first commit, with fastapi and requirents installed

---
+++ main.py
@@ -0,0 +1,15 @@
+import databases
+import sqlalchemy
+
+DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/store"
+
+data = databases.Database(DATABASE_URL)
+metadata = sqlalchemy.MetaData()
+
+books = sqlalchemy.Table(
+    "books",
+    metadata,
+    sqlalchemy.Column("id", sqlalchemy.Integer, primary_key=True),
+    sqlalchemy.Column("title", sqlalchemy.String),
+    sqlalchemy.Column("author", sqlalchemy.String),
+)
