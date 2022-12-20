25640da053764c12719cdd2afcd89db32c10b71e
Victor de Britto Ferreira <victorbrittoferreira@gmail.com>
2022-12-20 13:37:52 -0300

    decouple installed

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import databases
 from fastapi import FastAPI, Request
 import sqlalchemy
+from decouple import config
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/store"
+DATABASE_URL = (f"postgresql://{config('DB_USER')}:{config('DB_PASSWORD')}@localhost:{config('DB_PORT')}/{config('DB_NAME')}")
+
 
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
